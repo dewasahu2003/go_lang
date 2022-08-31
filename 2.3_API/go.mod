8d58db7094dd78c7bbb99b4c0cd8c01e9161df99
dewasahu2003 <dewasahu2003@gmail.com>
2022-09-01 01:39:49 +0530

    🤗 api....

---
+++ 2.3_API/go.mod
@@ -0,0 +1,5 @@
+module github.com/dewasahu2003/go_api
+
+go 1.18
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- 2.3_API/main.go
+++ 2.3_API/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Courses struct {
+	CourseId    string  `json:"courseid"`
+	CourseName  string  `json:"coursename"`
+	CoursePrice int     `json:"courseprice"`
+	Author      *Author `json:"author"`
+}
+
+type Author struct {
+	FullName string `json:"fullname"`
+	Website  string `json:"website"`
+}
+
+//fake database
+var courses []Courses
+
+//middleware,helper
+func (c Courses) IsEmpty() bool {
+	return c.CourseId == "" && c.CourseName == ""
+}
 
 func main() {
 	fmt.Println("API")
+}
+
+//controller
+
+//home route
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>change the order</h1>"))
+}
+
+func allcourses(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("All Courses")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(courses)
 }
