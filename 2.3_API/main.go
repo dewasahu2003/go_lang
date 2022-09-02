b50904b905f752ff8b5f4c2e0236f67461ee248c
dewasahu2003 <dewasahu2003@gmail.com>
2022-09-03 01:11:02 +0530

    reading req(by mux),writind resp(by json encoder)

--- 2.3_API/main.go
+++ 2.3_API/main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 type Courses struct {
@@ -22,7 +24,7 @@ type Author struct {
 var courses []Courses
 
 //middleware,helper
-func (c Courses) IsEmpty() bool {
+func (c *Courses) IsEmpty() bool {
 	return c.CourseId == "" && c.CourseName == ""
 }
 
@@ -33,13 +35,37 @@ func main() {
 //controller
 
 //home route
-
+// w:= POST type of method
+//r:= GET type of method
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("<h1>change the order</h1>"))
 }
 
-func allcourses(w http.ResponseWriter, r *http.Request) {
+func getallcourses(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("All Courses")
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(courses)
+}
+
+func get_oneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("🎊-Getting one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	//getting what is passed in REQUEST
+	params := mux.Vars(r)
+	fmt.Println("params: ", params)
+
+	//querying the db
+	for _, course := range courses {
+		if course.CourseId == params["id"] {
+			//give a RESPONSE
+			json.NewEncoder(w).Encode(course) //giving |course| as response
+			return
+		} else {
+			json.NewEncoder(w).Encode("no course found 💃")
+			return
+		}
+
+	}
+
 }
