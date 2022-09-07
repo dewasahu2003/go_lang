23861ae039f1c019778f67a5d3281cc6d3855cb6
dewasahu2003 <dewasahu2003@gmail.com>
2022-09-07 11:58:12 +0530

    🌿mongo CRUD

--- 2.4_Mongodb/controller/controller.go
+++ 2.4_Mongodb/controller/controller.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/dewasahu2003/mongodbAPI/model"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -34,4 +37,57 @@ func init() {
 	collection = client.Database(dbname).Collection(colName)
 	//passing the collection as var
 
+}
+
+// we will define two set of helper method
+//one for client side processing
+//and second for mongoDB for CRUD operations
+
+//mongoDB Insert 1 record
+
+func mongo_insertMovie(movie model.Netflix) {
+	inserted, err := collection.InsertOne(context.Background(), movie)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("movie inserted in db with id:%v", inserted.InsertedID)
+}
+
+func mongo_updateMovie(movieId string) {
+	id, _ := primitive.ObjectIDFromHex(movieId)
+	filter := bson.M{"_id": id}                         //searching criteria
+	update := bson.M{"$set": bson.M{"iswatched": true}} //updating value
+
+	result, err := collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("The _id updated is:%+v", result.ModifiedCount)
+
+}
+
+func mongo_deleteMovie(movieId string) {
+	id, _ := primitive.ObjectIDFromHex(movieId)
+	filter := bson.M{"_id": id}
+
+	deleteResult, err := collection.DeleteOne(context.Background(), filter)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("record id:%+v\n delete count:%+v", id, deleteResult)
+}
+
+func mongo_deleteAllMovie() {
+	filter := bson.D{{}}
+
+	deleteResult, err := collection.DeleteMany(context.Background(), filter, nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("all records deleted:%+v", deleteResult.DeletedCount)
 }
